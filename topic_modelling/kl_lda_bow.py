bfa6cff47d183f2371790cfaee2b7ad06d37528c
kristyylee <kristylee@u.nus.edu>
2023-03-28 22:24:15 +0800

    update nb

--- topic_modelling/kl_lda_bow.py
+++ topic_modelling/kl_lda_bow.py
@@ -10,6 +10,9 @@ import os
 from pathlib import Path
 from gensim.models import CoherenceModel
 import numpy as np
+from sklearn.model_selection import train_test_split
+import warnings
+warnings.filterwarnings("ignore", category=DeprecationWarning)
 
 """
 # split a sentence into a list 
@@ -37,7 +40,7 @@ def split_sentence(text):
 def compute_coherence_values(type_corpus, k):
     topics = []
     score = []
-    for i in range(2, k):
+    for i in range(1, k):
         lda_model = gensim.models.LdaMulticore(corpus=type_corpus,
                                                id2word=id2word,
                                                 num_topics=k, 
@@ -61,6 +64,22 @@ def compute_coherence_values(type_corpus, k):
     # return the number of topics with the highest coherence score
     return [topics[max_index], max(score)]
 
+def preprocess_words(data_inv):
+    # split sentences to individual words
+    data_word = split_sentence(data_inv)
+
+    # Create dictionary 
+    id2words = corpora.Dictionary(data_word)
+
+    # Create Corpus
+    collection_texts = data_word
+
+    # Term Document Frequency
+    # convert list of words into bag-of-words format
+    bow_corpuss = [id2words.doc2bow(text) for text in collection_texts]
+
+    return ([data_word, id2words, bow_corpuss])
+
 if __name__ == '__main__':
     # read in post processed data
     processed_data = pd.read_csv('../data/curated/reviews/yiting_cleaned_reviews.csv')
@@ -68,23 +87,17 @@ if __name__ == '__main__':
     # combine all the processed text into a list
     data = processed_data.processed_text.values.tolist()
 
-    # split sentences to individual words
-    data_words = split_sentence(data)
+    # split test-train in ratio 20:80
+    # included random state parameter so that results are reproducible
+    training_data, testing_data = train_test_split(data, test_size =0.2, random_state=25)
 
-    # Create dictionary 
-    id2word = corpora.Dictionary(data_words)
-
-    # Create Corpus
-    collection_texts = data_words
-
-    # Term Document Frequency
-    # convert list of words into bag-of-words format
-    bow_corpus = [id2word.doc2bow(text) for text in collection_texts]
-
+    # preprocess words
+    data_words, id2word, bow_corpus = preprocess_words(training_data)
+ 
     # number of topics
     num_topics = 5
 
-    # Running LDA using Bag of Words 
+    # Running LDA using Bag of Words for training data
     lda_model = gensim.models.LdaMulticore(corpus=bow_corpus,
                                         id2word=id2word,
                                         num_topics=num_topics)
@@ -107,11 +120,37 @@ if __name__ == '__main__':
     final_lda_model = gensim.models.LdaMulticore(corpus=bow_corpus,
                                         id2word=id2word,
                                         num_topics=final_num_topics)
-    
-    # Print the Keyword in the 5 topics 
+    # save trained model
+    final_lda_model.save('../model/kl_lda_bow_model.pkl')
+   
+    # Print the Keyword in optimal model
     for idx, topic in final_lda_model.print_topics(num_words=10):    
         print('Topic: {} \nWords: {}'.format(idx, topic))
-    
+
+    # Use test data on trained LDA_model
+    # split test data sentences to individual words
+    data_word_test = split_sentence(testing_data)
+
+    # Create Corpus for test data using trained dictionary
+    collection_text_test = data_word_test
+
+    # convert list of words into bag-of-words format
+    bow_corpus_test = [id2word.doc2bow(text) for text in collection_text_test]
+
+    # load model 
+    lda_model_test = gensim.models.LdaMulticore.load('../model/kl_lda_bow_model.pkl')
+    #trial1 = lda_model_test.get_document_topics(bow_corpus_test)
+    a = []
+    for query in bow_corpus_test:
+        for i in lda_model_test[query]:
+            a.append(i)
+    b = list(sorted(a, key=lambda x: x[1]))
+    b.reverse()
+    print(b[:10])
+    for j in range(final_num_topics):
+        print(lda_model_test.print_topic(b[j][0]), '\n')   
+
+    """
     # Visualize the topics
     LDAvis_data_filepath = os.path.join('../model/kl_ldavis_bow_'+str(final_num_topics)+'.pkl')
     filePath = Path(LDAvis_data_filepath)
@@ -124,7 +163,11 @@ if __name__ == '__main__':
     with open(LDAvis_data_filepath, 'rb') as f:
         LDAvis_prepared = pickle.load(f)
     pyLDAvis.save_html(LDAvis_prepared, 'kl_ldavis_bow_'+str(final_num_topics)+'.html')
-    LDAvis_prepared
+    """
+
+    
+
+    
         
 
 
