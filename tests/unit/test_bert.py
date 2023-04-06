5239b439ce4fbdb45adff68b404421fbc8c9e214
anqi-1 <e0425810@u.nus.edu>
2023-04-06 16:13:36 +0800

    function to test labels and tokenization

--- tests/unit/test_bert.py
+++ tests/unit/test_bert.py
@@ -1,12 +1,29 @@
 import pytest
 
-from sentiment_analysis.Deep_Learning.bert import read_data
+from sentiment_analysis.Deep_Learning.bert import read_data, labels_array, tokenization
 
-def test_read_input_file():
+@pytest.fixture
+def path_to_input_data():
+    path = "../../data/curated/reviews/cleaned_reviews.csv"
+    return path
+
+def test_read_input_file(path_to_input_data):
     """
     Test to Check whether the code reads a file correctly from a specified path and outputs correct columns
     """
-    input_train_data = read_data("../../data/curated/reviews/cleaned_reviews.csv")
+    input_train_data = read_data(path_to_input_data)
 
     assert 'labels' in input_train_data.columns
     assert 'text' in input_train_data.columns
+
+def test_labels_tokenization(path_to_input_data):
+    """Test to check output of labels_array and tokenization function output have the same length
+    """
+    data = read_data(path_to_input_data)
+    labels = labels_array(data)
+    Xids, Xmask = tokenization(data)
+    assert len(labels) == len(Xids)
+    assert len(labels) == len(Xmask)
+    assert len(Xmask) == len(Xids)
+
+
