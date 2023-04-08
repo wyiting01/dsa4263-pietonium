d47b721ec387b014b6c4c6c99362881709b7ebac
Yi Ting <yiting5890@gmail.com>
2023-04-08 20:53:19 +0800

    added evaluation with models other than saved weights in evaluate.py

--- deployment/evaluate.py
+++ deployment/evaluate.py
@@ -102,3 +102,31 @@ def evaluate(models_meta, test_data, target_models = ["svm", "xgboost", "bert"])
         result_dict[model] = model_result_dict
 
     return result_dict
+
+def evaluate_with_models(x_test, y_test, target_models):
+    result_dict = {}
+    for model_name, model in target_models:
+        model_result_dict = {}
+        if model_name != "bert":
+            predicted_y = model.predict(x_test)
+            predicted_prob_y = model.predict_proba(x_test)
+            model_result_dict["prediction"] = predicted_y
+            model_result_dict["prediction_prob"] = predicted_prob_y
+
+        metric_dict = {}
+        cm_2d = confusion_matrix(y_test, predicted_y)
+        cm = cm_2d.ravel() # tn, fp, fn, tp
+        accuracy = accuracy_score(y_test, predicted_y) # (tp+tn)/(tp+fp+tn+fn)
+        precision = cm[3] / (cm[3] + cm[1]) # tp/(tp+fp)
+        recall = cm[3] / (cm[3] + cm[2]) # tp/(tp+fn)
+        metric_dict["cm"] = cm_2d
+        metric_dict["accuracy"] = accuracy
+        metric_dict["precision"] = precision
+        metric_dict["recall"] = recall
+
+        # update result dict
+        model_result_dict["metrics"] = metric_dict
+
+        # update to result dict
+        result_dict[model_name] = model_result_dict
+    return result_dict
